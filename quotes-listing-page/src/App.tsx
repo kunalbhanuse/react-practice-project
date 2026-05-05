204b16be93554c130507ea791b9f58d78ed29655
kunal bhanuse <kunalbhanuse07@gmail.com>
2026-05-05 18:51:20 +0530

    quotes-listing-application done

---
+++ quotes-listing-page/src/App.tsx
@@ -0,0 +1,11 @@
+import QuotesPage from "./components/QuotesPage.tsx";
+
+function App() {
+  return (
+    <>
+      <QuotesPage />
+    </>
+  );
+}
+
+export default App;
---
+++ quotes-listing-page/src/components/QuotesPage.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./quotes.css";
+
+function QuotesPage() {
+  const [quotes, setQuotes] = useState([]);
+  useEffect(() => {
+    async function quatesFeatch() {
+      const res = await axios.get(
+        "https://api.freeapi.app/api/v1/public/quotes",
+      );
+      setQuotes(res.data.data.data);
+    }
+    quatesFeatch();
+  }, []);
+  return (
+    <div className="wrappper">
+      <h1>Quotes :</h1>
+      <div className="container">
+        {quotes.map((quote) => (
+          <div className="card" key={quote.id}>
+            <p>{quote.author}</p>
+            <p>{quote.content}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default QuotesPage;
